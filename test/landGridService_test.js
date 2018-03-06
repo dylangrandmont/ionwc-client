b7a0ab943a16a2f77a386a136090bc28e9a75ef7
dylangrandmont <dylan.grandmont@calgaryscientific.com>
2018-03-05 19:39:00 -0700

    Add unit tests for zoom 11 and group zoom 13 tests

--- test/landGridService_test.js
+++ test/landGridService_test.js
@@ -14,6 +14,9 @@ describe("landGridService", function() {
         lng: -102.0
       }
     });
+
+    landGridService.setMap(map);
+    landGridService.toggleLandGrid(false);
   });
 
   it("should zoom in map if toggled on", function() {
@@ -68,114 +71,18 @@ describe("landGridService", function() {
     expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
   });
 
-  it("should display e1w1SectionLayer if zoomed to 13 near first meridian", function() {
-    map.setZoom(13);
+  it("should display w112NTSLayer layer if zoomed to 11 between 122, 120 lng", function() {
+    map.setZoom(11);
     map.setCenter({
       lat: 50.0,
-      lng: -97.45
+      lng: -121.45
     });
     landGridService.setMap(map);
     landGridService.toggleLandGrid(true);
 
-    expect(map.getZoom()).toEqual(13);
+    expect(map.getZoom()).toEqual(11);
     expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
     expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(map);
-    expect(LayerManager.townshipLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
-    expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
-    expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
-    expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
-  });
-
-  it("should display w1w2SectionLayer if zoomed to 13 near second meridian", function() {
-    map.setZoom(13);
-    map.setCenter({
-      lat: 50.0,
-      lng: -102.0
-    });
-    landGridService.setMap(map);
-    landGridService.toggleLandGrid(true);
-
-    expect(map.getZoom()).toEqual(13);
-    expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(map);
-    expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.townshipLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
-    expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
-    expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
-    expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
-  });
-
-  it("should display w2w3SectionLayer if zoomed to 13 near third meridian", function() {
-    map.setZoom(13);
-    map.setCenter({
-      lat: 50.0,
-      lng: -106.0
-    });
-    landGridService.setMap(map);
-    landGridService.toggleLandGrid(true);
-
-    expect(map.getZoom()).toEqual(13);
-    expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(map);
-    expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.townshipLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
-    expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
-    expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
-    expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
-  });
-
-  it("should display w3w4SectionLayer if zoomed to 13 near fourth meridian", function() {
-    map.setZoom(13);
-    map.setCenter({
-      lat: 50.0,
-      lng: -110.0
-    });
-    landGridService.setMap(map);
-    landGridService.toggleLandGrid(true);
-
-    expect(map.getZoom()).toEqual(13);
-    expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(map);
-    expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.townshipLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
-    expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
-    expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
-    expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
-    expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
-  });
-
-  it("should display w4w5SectionLayer if zoomed to 13 near fifth meridian", function() {
-    map.setZoom(13);
-    map.setCenter({
-      lat: 50.0,
-      lng: -114.0
-    });
-    landGridService.setMap(map);
-    landGridService.toggleLandGrid(true);
-
-    expect(map.getZoom()).toEqual(13);
-    expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
-    expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(map);
     expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
     expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
     expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
@@ -184,22 +91,21 @@ describe("landGridService", function() {
     expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
     expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
     expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
-    expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+    expect(LayerManager.w112NTSLayer.getMap()).toEqual(map);
     expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
   });
 
-
-  it("should display w5w6SectionLayer if zoomed to 13 near sixth meridian", function() {
-    map.setZoom(13);
+  it("should display w122NTSLayer layer if zoomed to 11 between 124, 122 lng", function() {
+    map.setZoom(11);
     map.setCenter({
       lat: 50.0,
-      lng: -118.0
+      lng: -123.45
     });
     landGridService.setMap(map);
     landGridService.toggleLandGrid(true);
 
-    expect(map.getZoom()).toEqual(13);
-    expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(map);
+    expect(map.getZoom()).toEqual(11);
+    expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
     expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
     expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
     expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
@@ -210,7 +116,147 @@ describe("landGridService", function() {
     expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
     expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
     expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
-    expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    expect(LayerManager.w122NTSLayer.getMap()).toEqual(map);
+  });
+
+  describe("when zoomed to 13", function() {
+
+    beforeEach(function() {
+      map.setZoom(13);
+    });
+
+    it("should display e1w1SectionLayer if near first meridian", function() {
+      map.setCenter({
+        lat: 50.0,
+        lng: -97.45
+      });
+      landGridService.setMap(map);
+      landGridService.toggleLandGrid(true);
+
+      expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(map);
+      expect(LayerManager.townshipLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
+      expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
+      expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+      expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    });
+
+    it("should display w1w2SectionLayer if near second meridian", function() {
+      map.setCenter({
+        lat: 50.0,
+        lng: -102.0
+      });
+      landGridService.setMap(map);
+      landGridService.toggleLandGrid(true);
+
+      expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(map);
+      expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.townshipLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
+      expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
+      expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+      expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    });
+
+    it("should display w2w3SectionLayer if near third meridian", function() {
+      map.setCenter({
+        lat: 50.0,
+        lng: -106.0
+      });
+      landGridService.setMap(map);
+      landGridService.toggleLandGrid(true);
+
+      expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(map);
+      expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.townshipLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
+      expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
+      expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+      expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    });
+
+    it("should display w3w4SectionLayer if near fourth meridian", function() {
+      map.setCenter({
+        lat: 50.0,
+        lng: -110.0
+      });
+      landGridService.setMap(map);
+      landGridService.toggleLandGrid(true);
+
+      expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(map);
+      expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.townshipLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
+      expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
+      expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+      expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    });
+
+    it("should display w4w5SectionLayer if near fifth meridian", function() {
+      map.setCenter({
+        lat: 50.0,
+        lng: -114.0
+      });
+      landGridService.setMap(map);
+      landGridService.toggleLandGrid(true);
+
+      expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(map);
+      expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.townshipLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
+      expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
+      expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+      expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    });
+
+
+    it("should display w5w6SectionLayer if sixth meridian", function() {
+      map.setCenter({
+        lat: 50.0,
+        lng: -118.0
+      });
+      landGridService.setMap(map);
+      landGridService.toggleLandGrid(true);
+
+      expect(LayerManager.w5w6SectionLayer.getMap()).toEqual(map);
+      expect(LayerManager.w4w5SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w3w4SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w2w3SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.w1w2SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.e1w1SectionLayer.getMap()).toEqual(null);
+      expect(LayerManager.townshipLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts50kLayer.getMap()).toEqual(map);
+      expect(LayerManager.nts250kLayer.getMap()).toEqual(map);
+      expect(LayerManager.ntsBlockLayer.getMap()).toEqual(map);
+      expect(LayerManager.w112NTSLayer.getMap()).toEqual(null);
+      expect(LayerManager.w122NTSLayer.getMap()).toEqual(null);
+    });
   });
 
   it("should not display any layers if toggled off", function() {
